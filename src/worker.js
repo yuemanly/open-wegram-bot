d498826c1a1a47506643c41e90f9a665ae1cf2fc
yuemanly <yuemanly@163.com>
2025-05-08 16:17:52 +0800

    Update worker.js
    不知道为啥总提示token没有16位，算了自动生成一个算球

--- src/worker.js
+++ src/worker.js
@@ -5,15 +5,45 @@
  * GitHub Repository: https://github.com/wozulong/open-wegram-bot
  */
 
+// import {handleRequest} from './core.js';
+
+// export default {
+//     async fetch(request, env, ctx) {
+//         const config = {
+//             prefix: env.PREFIX || 'public',
+//             secretToken: env.SECRET_TOKEN || ''
+//         };
+
+//         return handleRequest(request, config);
+//     }
+// };
+
+/**
+ * Open Wegram Bot - Cloudflare Worker Entry Point
+ * A two-way private messaging Telegram bot
+ *
+ * GitHub Repository: https://github.com/wozulong/open-wegram-bot
+ */
+
 import {handleRequest} from './core.js';
 
 export default {
     async fetch(request, env, ctx) {
         const config = {
             prefix: env.PREFIX || 'public',
-            secretToken: env.SECRET_TOKEN || ''
+            secretToken: env.SECRET_TOKEN || generateToken(16)
         };
 
         return handleRequest(request, config);
     }
 };
+
+// Helper function to generate a random token
+function generateToken(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < length; i++) {
+        token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
+}
